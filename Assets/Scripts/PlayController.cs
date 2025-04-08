d98d5a89578f6e5f8121b1e6f620e07e24cd7bcf
SiyeongLee <happyamong123@naver.com>
2025-04-08 16:24:06 +0900

    [Feat] 기본 시스템 완성 업로드

--- Assets/Scripts/PlayController.cs
+++ Assets/Scripts/PlayController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,14 +10,21 @@ public class PlayController : MonoBehaviour
     public float jumpForce = 5f;
     public Transform groundCheck;
     public LayerMask groundLayer;
-
-    public Rigidbody2D rb;
     private bool isGrounded;
+    private bool isGiant = false;
+    public Rigidbody2D rb;
+    private Animator pAni;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        pAni = GetComponent <Animator>();
     }
+    
+      
+
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -30,11 +38,32 @@ public class PlayController : MonoBehaviour
         float moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
+        if (moveInput < 0)
+            transform.localScale = new Vector3(1f,1f,1f);
+        if (moveInput > 0)
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+
+        if (isGiant)
+        {
+            if (moveInput < 0)
+                transform.localScale = new Vector3(2f,2f,1f);
+            if (moveInput > 0)
+                transform.localScale = new Vector3(-2f, 2f, 1f);
+            else
+            {
+                if (moveInput < 0)
+                    transform.localScale = new Vector3(1f,1f,1f);
+                if (moveInput > 0)
+                transform.localScale = new Vector3(-1f,1f,1f);
+            }
+        }
+
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
 
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            pAni.SetTrigger("JumpAction");
         }
 
     }
@@ -51,6 +80,15 @@ public class PlayController : MonoBehaviour
             collision.GetComponent<LevelObject>().MoveToNextLevel();
         }
 
+        if (collision.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (collision.CompareTag("Item"))
+        {
+            isGiant = true;
+            Destroy(collision.gameObject);
+        }
     }
 }
 
